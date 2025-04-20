72b137aadda65f2dd6219a9888ea2373b344f1be
Noi MC <yutynetwork2011@gmail.com>
2025-04-20 18:22:04 +0700

    Create load.sh

---
+++ load.sh
@@ -0,0 +1,59 @@
+clear
+echo "🄽🄾🄸 🄼🄲"
+sleep 1
+echo "Loading ."
+sleep 1
+echo "Loading .."
+sleep 1
+echo "Loading ..."
+sleep 1
+echo "Done"
+sleep 3
+clear
+echo "🄽🄾🄸 🄼🄲"
+echo "Choose whether to agree to create a server or not."
+echo "Enter/Ctrl+C"
+read -p ">> "
+sleep 2
+clear
+echo "🄽🄾🄸 🄼🄲"
+echo "Good"
+sleep 3
+clear
+echo "🄽🄾🄸 🄼🄲"
+apt install wget -y
+cd ~/plugins
+wget https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/5.3.2/PAPER/ViaVersion-5.3.2.jar
+wget https://hangarcdn.papermc.io/plugins/ViaVersion/ViaBackwards/versions/5.3.2/PAPER/ViaBackwards-5.3.2.jar
+wget https://cdn.modrinth.com/data/wKkoqHrH/versions/FyVa6ZMD/Geyser-Spigot.jar
+curl https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest/downloads/spigot
+sleep 2
+cd ~/
+clear
+echo "🄽🄾🄸 🄼🄲"
+echo "Good"
+sleep 2
+clear
+echo "🄽🄾🄸 🄼🄲"
+echo "Start"
+sleep 1
+echo "Loading ."
+clear
+sleep 3
+echo "🄽🄾🄸 🄼🄲"
+echo "Start"
+echo "Loading ."
+sleep 1
+echo "Loading .."
+sleep 3
+clear
+echo "🄽🄾🄸 🄼🄲"
+echo "Start"
+echo "Loading ."
+echo "Loading .."
+sleep 1
+echo "Loading ..."
+sleep 5
+clear
+echo "🄽🄾🄸 🄼🄲"
+echo "Done"
